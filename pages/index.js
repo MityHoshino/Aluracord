0fecc2af56a8a035075222963e67888970d3616e
Mity Hoshino <mity.hoshino@gmail.com>
2022-01-29 22:23:34 -0300

    day2

--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,7 @@
 import appConfig from '../config.json'
 import { Box, Button, Text, TextField, Image } from '@skynexui/components';
-
-function GlobalStyle() {
-    return (
-      <style global jsx>{`
-        * {
-          margin: 0;
-          padding: 0;
-          box-sizing: border-box;
-          list-style: none;
-        }
-        body {
-          font-family: 'Open Sans', sans-serif;
-        }
-        /* App fit Height */ 
-        html, body, #__next {
-          min-height: 100vh;
-          display: flex;
-          flex: 1;
-        }
-        #__next {
-          flex: 1;
-        }
-        #__next > * {
-          flex: 1;
-        }
-        /* ./App fit Height */ 
-      `}</style>
-    );
-  }
+import React from 'react';
+import {useRouter} from 'next/router';
 
 function Titulo(props){
     console.log(props);
@@ -61,16 +34,17 @@ function Titulo(props){
 // export default HomePage
 
 export default function PaginaInicial() {
-    const username = 'peas';
-  
+    // const username = 'peas';
+    const [username, setUsername] = React.useState('');
+    const roteamento = useRouter();
+
     return (
       <>
-        <GlobalStyle />
         <Box
           styleSheet={{
             display: 'flex', alignItems: 'center', justifyContent: 'center',
-            backgroundColor: appConfig.theme.colors.primary[500],
-            backgroundImage: 'url(https://virtualbackgrounds.site/wp-content/uploads/2020/08/the-matrix-digital-rain.jpg)',
+            // backgroundColor: appConfig.theme.colors.primary[500],
+            backgroundImage: 'url(https://wallpapercave.com/wp/wp2001518.jpg)',
             backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
           }}
         >
@@ -92,6 +66,12 @@ export default function PaginaInicial() {
             {/* Formulário */}
             <Box
               as="form"
+              onSubmit={function(event){
+                event.preventDefault();
+                console.log('clicou');
+                // window.location.href = '/chat';
+                roteamento.push('/chat');
+              }}
               styleSheet={{
                 display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center',
                 width: { xs: '100%', sm: '50%' }, textAlign: 'center', marginBottom: '32px',
@@ -102,7 +82,19 @@ export default function PaginaInicial() {
                 {appConfig.name}
               </Text>
   
+              {/* <input
+                type="text"
+                value={username}
+                onChange={function(event){
+                  console.log('usuario digitou',setUsername(event.target.value));
+                }}
+              /> */}
+
               <TextField
+                value={username}
+                onChange={function(event){
+                  console.log('usuario digitou',setUsername(event.target.value));
+                }}
                 fullWidth
                 textFieldColors={{
                   neutral: {
